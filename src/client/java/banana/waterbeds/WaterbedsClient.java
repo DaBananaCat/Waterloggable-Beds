322c6f51ec53af9b068779a7304330be7dd5b6d3
Sir_BananaCat <connor@happychaps.com>
2025-01-24 16:18:07 -0600

    Upload Template

---
+++ src/client/java/banana/waterbeds/WaterbedsClient.java
@@ -0,0 +1,10 @@
+package banana.waterbeds;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WaterbedsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}

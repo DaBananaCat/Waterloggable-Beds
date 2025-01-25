de56c9232b5b6d3233443287dfb869cf3f973330
Sir_BananaCat <connor@happychaps.com>
2025-01-25 16:20:21 -0600

    Block is now waterloggable

--- src/main/java/banana/waterbeds/Blocks/WaterbedBlock.java
+++ src/main/java/banana/waterbeds/Blocks/WaterbedBlock.java
@@ -1,17 +1,54 @@
 package banana.waterbeds.Blocks;
 
 
+import com.mojang.serialization.Decoder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.BedBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class WaterbedBlock extends BedBlock {
-
+public class WaterbedBlock extends BedBlock implements SimpleWaterloggedBlock {
+    public static final BooleanProperty WATERLOGGED;
+    static {
+        WATERLOGGED = BlockStateProperties.WATERLOGGED;
+    }
     public WaterbedBlock() {
         super(DyeColor.WHITE, Properties.ofFullCopy(Blocks.WHITE_BED));
+        this.registerDefaultState((BlockState) this.defaultBlockState().setValue(WATERLOGGED, false));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED);
+        super.createBlockStateDefinition(builder);
+    }
+
+    public boolean placeLiquid(LevelAccessor level, BlockPos pos, BlockState state, FluidState fluidState) {
+        return SimpleWaterloggedBlock.super.placeLiquid(level, pos, state, fluidState);
+    }
+
+    public boolean canPlaceLiquid(@Nullable Player player, BlockGetter level, BlockPos pos, BlockState state, Fluid fluid) {
+        return SimpleWaterloggedBlock.super.canPlaceLiquid(player, level, pos, state, fluid);
+    }
+
+    protected @NotNull FluidState getFluidState(BlockState state) {
+        return (Boolean)state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
     }
 
     @Override
